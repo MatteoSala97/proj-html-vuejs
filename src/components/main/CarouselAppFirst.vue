c1c2c869c0f00d659887d0b2bf3cf83a98443dea
Matteo Sala <matteo111997@gmail.com>
2024-02-21 00:43:34 +0100

    Working on the carousel

--- src/components/main/CarouselAppFirst.vue
+++ src/components/main/CarouselAppFirst.vue
@@ -3,18 +3,53 @@ export default {
   name: "CarouselAppFirst",
   data() {
     return {
+      currentIndex: 0,
+      timer: null,
       slides:[
         {
-          image: 'src/assets/img/process1.png'
-        }
+          image: "src/assets/img/process1.png",
+          phrase: "We'll take your idea and create a technical script which consists of action notes and animation description",
+          badge: "1"
+        },
+        {
+          image: "src/assets/img/process2.png",
+          phrase: "We'll take your idea and create a technical script which consists of action notes and animation description",
+          badge: "2"
+        },
+        {
+          image: "src/assets/img/process1.png",
+          phrase: "We'll take your idea and create a technical script which consists of action notes and animation description",
+          badge: "3"
+        },
+        {
+          image: "src/assets/img/process2.png",
+          phrase: "We'll take your idea and create a technical script which consists of action notes and animation description",
+          badge: "4"
+        },
       ]
     };
   },
-  created() {
-    
+  mounted: function() {
+    this.startSlide()
   },
   methods: {
-    
+    startSlide: function(){
+      this.timer = setInterval(this.next, 500)
+    },
+    next: function(){
+      if(this.currentIndex + 1 == this.slides.length){
+        this.currentIndex = 0
+      } else{
+        this.currentIndex = this.currentIndex + 1
+      }
+    },
+    prev: function(){
+      if((this.currentIndex - 1) < 0){
+        this.currentIndex = this.slides.length - 1
+      } else{
+        this.currentIndex = this.currentIndex - 1
+      }
+    }
   }
 };
 </script>
@@ -33,24 +68,27 @@ export default {
                 <p>We have an effective process for working on animation</p>
             </div>
             <div class="row">
-                <div class="col-5 d-flex flex-column align-items-center text-center border rounded-4 p-4" id="card">
-                    <img src="../../assets/img/process1.png" alt="1">
+                <div class="col-5 d-flex flex-column align-items-center text-center border rounded-4 p-4" id="card"
+                v-for="i in [currentIndex]" :key="i"
+                >
+                    <img :src="slides[i].image" alt="1">
                     <h2 class="mt-2">Scripting</h2>
                     <p>We'll take your idea and create a technical script which consists of action notes and animation description</p>
                     <span id="badge">1</span>
-                </div>
-                <div class="col-5 offset-2 d-flex flex-column align-items-center text-center border rounded-4 p-4" id="card">
-                    <img src="../../assets/img/process1.png" alt="2">
+                  </div>
+                  <div class="col-5 offset-1 d-flex flex-column align-items-center text-center border rounded-4 p-4" id="card"
+                  v-for="i in [currentIndex]" :key="i">
+                    <img :src="slides[i+1].image" alt="2">
                     <h2 class="mt-2">Pre-Production</h2>
                     <p>We'll take your idea and create a technical script which consists of action notes and animation description</p>
                     <span id="badge">2</span>
-                </div>
+                  </div>
                 <div class="d-flex gap-3 justify-content-start mt-4">
                     <button>
-                        <i class="fa-solid fa-arrow-left border rounded-circle p-3"></i>
+                        <i class="fa-solid fa-arrow-left border rounded-circle p-3" @click="prev"></i>
                     </button>
                     <button>
-                        <i class="fa-solid fa-arrow-right border rounded-circle p-3"></i>
+                        <i class="fa-solid fa-arrow-right border rounded-circle p-3" @click="next"></i>
                     </button>
                 </div>
             </div>
