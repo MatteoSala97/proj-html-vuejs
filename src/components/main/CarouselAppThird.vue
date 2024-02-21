fa25e5e1b9b3129e28c6f03f4a086d200c32941c
Matteo Sala <matteo111997@gmail.com>
2024-02-21 10:29:26 +0100

    Animated this as well

--- src/components/main/CarouselAppThird.vue
+++ src/components/main/CarouselAppThird.vue
@@ -4,13 +4,39 @@ export default {
   name: "CarouselAppThird",
   data() {
     return {
-      
+      currentIndex: 0,
+      timer: null,
+      slides:[
+        "src/assets/img/sponsor1.png",
+        "src/assets/img/sponsor2.png",
+        "src/assets/img/sponsor3.png",
+        "src/assets/img/sponsor4.png",
+        "src/assets/img/sponsor5.png",
+        "src/assets/img/sponsor6.png"
+    ]
     };
   },
-  created() {
-    
+  mounted: function() {
+    this.startSlide()
   },
   methods: {
+    startSlide: function(){
+      this.timer = setInterval(this.next, 1000)
+    },
+    next: function(){
+      if(this.currentIndex === this.slides.length - 1){
+        this.currentIndex = 0
+      } else{
+        this.currentIndex ++
+      }
+    },
+    prev: function(){
+      if((this.currentIndex - 1) < 0){
+        this.currentIndex = this.slides.length - 1
+      } else{
+        this.currentIndex --
+      }
+    }
     
   }
 };
@@ -25,22 +51,22 @@ export default {
         </div>
         <div class="d-flex justify-content-around pt-5" id="small-container">
             <div class="d-flex flex-column align-items-center ">
-                <img src="../../assets/img/sponsor1.png" alt="1">
+                <img :src="slides[currentIndex]" alt="1">
             </div>
             <div>
-                <img src="../../assets/img/sponsor2.png" alt="2">
+                <img :src="slides[(currentIndex + 1) % slides.length]" alt="2">
             </div>
             <div>
-                <img src="../../assets/img/sponsor3.png" alt="3">
+                <img :src="slides[(currentIndex + 2) % slides.length]" alt="3">
             </div>
             <div>
-                <img src="../../assets/img/sponsor5.png" alt="4">
+                <img :src="slides[(currentIndex + 3) % slides.length]" alt="4">
             </div>
             <div>
-                <img src="../../assets/img/sponsor4.png" alt="5">
+                <img :src="slides[(currentIndex + 4) % slides.length]" alt="5">
             </div>
             <div>
-                <img src="../../assets/img/sponsor6.png" alt="6">
+                <img :src="slides[(currentIndex + 5) % slides.length]" alt="6">
             </div>
         </div>
     </div>
